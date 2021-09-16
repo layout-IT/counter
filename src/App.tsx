a0af065ba07567a41c0c8b6b5345abcff14be0b9
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-09-16 16:33:07 +0300

    redux with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React   from 'react';
+import React, {useEffect} from 'react';
 import './App.module.css';
 import {Counter} from "./Counter";
 import {useDispatch, useSelector} from "react-redux";
-import {addNumberAc, resetAc,} from "./bll/reduser";
+import {addNumberAc, resetAc, setMaxNumberOutOfLocalStorageAC, setMinNumberOutOfLocalStorageAC,} from "./bll/reduser";
 import {appStateType} from "./bll/store";
 
 function App() {
 
-    let value = useSelector<appStateType, number >(state => state.counterReduser.value)
+
+    let value = useSelector<appStateType, number>(state => state.counterReduser.value)
     const text = useSelector<appStateType, boolean>(state => state.counterReduser.text)
-    let maxValue = useSelector<appStateType, number >(state => state.counterReduser.maxValue)
+    let maxValue = useSelector<appStateType, number>(state => state.counterReduser.maxValue)
+    const startValue = useSelector<appStateType, number>(state => state.counterReduser.startValue)
     let dispatch = useDispatch()
 
 
-     const addNumber = () => {
-        if (text &&  maxValue >value){
+    const addNumber = () => {
+        if (text && maxValue > value) {
             dispatch(addNumberAc())
         }
 
@@ -22,32 +24,42 @@ function App() {
 
 
     const reset = (startValue: number) => {
-        if (text){
+        if (text) {
             dispatch(resetAc(startValue))
         }
 
     }
 
-    const addStartValueToInc = (startValue: number) => {
-        dispatch(addStartValueToInc(startValue))
-        // if (startValue >= 0) {
-        //     value = startValue
-        //     setState(value)
-        // }
+    // const addStartValueToInc = (startValue: number) => {
+    //     dispatch(addStartValueToInc(startValue))
+    // }
+    const locStorValue = () => {
+        localStorage.setItem('maxValue', maxValue.toString())
+        localStorage.setItem('startValue', startValue.toString())
+    }
 
-}
-
+    useEffect(() => {
+        let startValue = localStorage.getItem('startValue')
+        if (startValue) {
+            let parseMinValue = JSON.parse(startValue)
+            dispatch(setMinNumberOutOfLocalStorageAC(parseMinValue))
+        }
+        let maxValue = localStorage.getItem('maxValue')
+        if (maxValue) {
+            let parseMaxValue = JSON.parse(maxValue)
+            dispatch(setMaxNumberOutOfLocalStorageAC(parseMaxValue))
+        }
+    }, [])
     return (
         <div className="App">
             <Counter
-                state ={value}
-                 reset={reset}
+                state={value}
+                reset={reset}
                 text={text}
-                 addNumber={addNumber}
-                 addStartValueToInc={addStartValueToInc}
+                addNumber={addNumber}
+                locStorValue={locStorValue}
                 maxValue={maxValue}
-
-
+                startValue={startValue}
             />
         </div>
     );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,6 @@
 import s from './App.module.css'
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {maxValueDownAC, maxValueUpAC, setButtonAC, startValueDownAC, startValueUpAC} from "./bll/reduser";
-import {appStateType} from "./bll/store";
 
 
 export type CounterType = {
@@ -10,80 +8,43 @@ text: boolean
     state: number
     addNumber: () => void
     reset: (startValue: number) => void
-    addStartValueToInc: (startValue: number) => void
+    locStorValue: () => void
     maxValue: number
+    startValue: number
 }
 
 export const Counter = (props: CounterType) => {
     const dispatch = useDispatch()
+
     const maxValue = props.maxValue
-    const startValue = useSelector<appStateType, number>(state => state.counterReduser.startValue)
+    const startValue =  props.startValue
+
     const text = props.text
 
-    // let [maxValue, setmaxValue] = useState(0)
-    // let [startValue, setstartValue] = useState(0)
-    // let [text, setText] = useState(false)
 
-    // useEffect(() => {
-    //     let valueAsString = localStorage.getItem('startValue')
-    //     if (valueAsString) {
-    //         let newValue = JSON.parse(valueAsString)
-    //         setstartValue(newValue)
-    //     }
-    // }, [])
-    // useEffect(() => {
-    //     let valueAsString = localStorage.getItem('maxValue')
-    //     if (valueAsString) {
-    //         let newValue = JSON.parse(valueAsString)
-    //         setmaxValue(newValue)
-    //     }
-    // }, [])
-    //
-    //
-    // const locStorValue = () => {
-    //     localStorage.setItem('maxValue', maxValue.toString())
-    //     localStorage.setItem('startValue', startValue.toString())
-    // }
 
 
     const maxValueUp = () => {
         dispatch(maxValueUpAC())
     }
+
     const maxValueDown = () => {
         dispatch(maxValueDownAC())
-        // if (maxValue - 1 > startValue) {
-        //     setmaxValue(maxValue - 1)
-        //     setText(true)
-        // }
     }
 
     const startValueUp = () => {
         dispatch(startValueUpAC())
-        // if (maxValue > (startValue + 1)) {
-        //     setstartValue(startValue + 1)
-        //     setText(true)
-        // }
-
     }
+
     const startValueDown = () => {
         dispatch(startValueDownAC())
-
-        // if (maxValue > (startValue - 1)) {
-        //     setstartValue(startValue - 1)
-        //     setText(true)
-        // }
     }
 
-
     const setButton = () => {
-        if (startValue >=0){
+        props.locStorValue()
+        if (props.startValue >=0){
             dispatch(setButtonAC())
         }
-
-         // setText(false)
-         // props.addStartValueToInc(startValue)
-
-        // locStorValue()
     }
 
     return <div className={s.wrapper}>
@@ -101,31 +62,31 @@ export const Counter = (props: CounterType) => {
                 </div>
                 <div className={s.bottom_string}>
                     <div className={s.max_value}> start value:
-                        <div className={startValue < 0 ? s.arrowRed : s.arrow}>
+                        <div className={props.startValue < 0 ? s.arrowRed : s.arrow}>
                             <span className={s.top_arrow} onClick={startValueUp}></span>
                             <span className={s.bottom_arrow} onClick={startValueDown}></span>
-                            <span className={startValue < 0 ? s.position_B : s.position_A}>{startValue}</span>
+                            <span className={props.startValue < 0 ? s.position_B : s.position_A}>{props.startValue}</span>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={s.window_button}>
-                <button className={startValue < 0 || maxValue < 0 ? s.unSet : s.set} onClick={setButton}>set</button>
+                <button className={props.startValue < 0 || maxValue < 0 ? s.unSet : s.set} onClick={setButton}>set</button>
             </div>
 
         </div>
         <div className={s.right}>
             <div
 
-                className={props.state === maxValue && maxValue !== 0 && text ? s.red : s.title}>{!text && startValue < 0 ? 'negative value ' : !text ? "click on the 'set'" : props.state}</div>
+                className={props.state === maxValue && maxValue !== 0 && text ? s.red : s.title}>{!text && props.startValue < 0 ? 'negative value ' : !text ? "click on the 'set'" : props.state}</div>
             <div className={s.obert}>
                 <div className={s.items}>
                     <button className={props.state > -1 && text ? s.inc : s.op} onClick={() => {props.addNumber()
                     }}>inc
                     </button>
-                    <button className={props.state !== startValue && startValue > -1 && text ? s.reset : s.opasity}
+                    <button className={props.state !== props.startValue && props.startValue > -1 && text ? s.reset : s.opasity}
                             onClick={() => {
-                                props.reset(startValue)
+                                props.reset(props.startValue)
                             }}>reset
                     </button>
                 </div>
